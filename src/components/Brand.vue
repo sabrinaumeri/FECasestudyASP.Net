4fb0b1982d3b4fbbcc4e18171de1a1f0776f5e45
Sabrina <76846419+sabrinaumeri@users.noreply.github.com>
2021-07-09 18:46:56 -0400

    lab 3 case study complete

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -10,11 +10,45 @@ style="text-align: left"
 optionLabel="name"
 optionValue="id"
 placeholder="Select Brand"
+@change="loadProductitems"
 />
+ <div style="margin-top:2vh;" v-if="state.productitems.length > 0">
+ <DataTable
+ :value="state.productitems"
+ :scrollable="true"
+ scrollHeight="60vh"
+ selectionMode="single"
+ class="p-datatable-striped"
+ >
+ <Column style="margin-right:-35vw;">
+ <template #body="slotProps" >
+ <img
+ :src="`${slotProps.data.graphicName}`"
+ :alt="slotProps.data.productName"
+ class="product-image"
+ />
+ </template>
+ </Column>
+ <Column field="description" header="Select an Item"></Column>
+ </DataTable>
+ </div>
+
+<!-- <DataTable
+ v-if="state.productitems.length > 0"
+ :value="state.productitems"
+ :scrollable="true"
+ scrollHeight="55vh"
+ selectionMode="single"
+ class="p-datatable-striped"
+ >
+ <Column field="description" header="Items"></Column>
+ </DataTable> -->
+
 </div>
 </template>
 <script>
 import { reactive, onMounted } from "vue";
+import { fetcher } from "../util/apiutil";
 export default {
 setup() {
 onMounted(() => {
@@ -23,22 +57,38 @@ loadBrands();
 let state = reactive({
 status: "",
 brands: [],
+productitems: [],
 selectedBrand: {},
 });
 const loadBrands = async () => {
-let url ="https://localhost:44352/api/Brand";
-try {
-state.status = "finding brands ..."
-let response = await fetch(`${url}`);
-state.brands = await response.json();
-state.status = `loaded ${state.brands.length} brands`;
-} catch (err) {
-console.log(err);
-state.status = `Error has occured: ${err.message}`;
-}
-};
+ try {
+ state.status = "loading brands...";
+ const payload = await fetcher(`Brand`);
+ if (payload.error === undefined) {
+ state.brands = payload;
+ state.status = `loaded ${state.brands.length} brands`;
+ } else {
+ state.status = payload.error;
+ }
+ } catch (err) {
+ console.log(err);
+ state.status = `Error has occured: ${err.message}`;
+ }
+ };
+ const loadProductitems = async () => {
+ try {
+ state.status = `finding productitems for category ${state.selectedBrand}...`;
+ let payload = await fetcher(`Productitem/${state.selectedBrand}`);
+ state.productitems = payload;
+ state.status = `loaded ${state.productitems.length} product items`;
+ } catch (err) {
+ console.log(err);
+ state.status = `Error has occured: ${err.message}`;
+ }
+ };
 return {
 state,
+loadProductitems,
 };
 }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
 import Dropdown from 'primevue/dropdown';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
 import 'primevue/resources/primevue.min.css';   //core css
 import 'primevue/resources/themes/md-light-indigo/theme.css'; // theme
@@ -13,6 +15,8 @@ const app = createApp(App)
 app.use(PrimeVue);
 app.component('Button', Button);
 app.component('Dropdown', Dropdown);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
 app.mount('#app');
 
 
